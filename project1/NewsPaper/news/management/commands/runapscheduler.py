272a610bdaccde3c415fe72e5c3571f1366f4632
ksyukhen33 <157539437+ksyukhen33@users.noreply.github.com>
2024-07-29 22:32:55 +0300

    Add files via upload

--- project1/NewsPaper/news/management/commands/runapscheduler.py
+++ project1/NewsPaper/news/management/commands/runapscheduler.py
@@ -11,19 +11,19 @@ from django.template.loader import render_to_string
 from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
+from django.utils import timezone
 
-
-from news.models import Posts, Category
+from news.models import Posts, Category, Subscription
 
 logger = logging.getLogger(__name__)
 
 
 def my_job():
-    today= datetime.datetime.now()
+    today= timezone.now()
     last_week= today - datetime.timedelta(days=7)
     posts = Posts.objects.filter(date_post__gte = last_week)
-    categories = set(posts.values('name_category', flat=True).all())
-    subscribers = set(Category.objects.filter(name__in=categories).values.all('subscribers__email', flat=True))
+    categories = set(posts.values_list('postCategory', flat=True))
+    subscribers = set(Subscription.objects.filter(Category__in=categories).values_list('user__email', flat=True))
     
     html_content= render_to_string(
         'daily_post.html',
@@ -57,7 +57,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(minute="03", hour="23"),
+            trigger=CronTrigger(minute="00", hour="18"),
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
@@ -67,7 +67,7 @@ class Command(BaseCommand):
         scheduler.add_job(
             delete_old_job_executions,
             trigger=CronTrigger(
-                day_of_week="wed", hour="23", minute="03"
+                day_of_week="fri", hour="18", minute="00"
             ),
             id="delete_old_job_executions",
             max_instances=1,
